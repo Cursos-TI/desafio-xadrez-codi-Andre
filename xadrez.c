a44b885c4395178076fe0e84e9838e131c8cd476
André Miranda <120103103+codi-Andre@users.noreply.github.com>
2025-02-05 14:54:42 +0000

    :sparkles: feat(desafio): conclui o nível aventureiro

--- xadrez.c
+++ xadrez.c
@@ -34,6 +34,14 @@ int main() {
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
+    for (int down = 2, left = 1; left > 0; left--)
+    {
+        while(down > 0) {
+            printf("Cavalo: Baixo\n");
+            down--;
+        };
+        printf("Cavalo: Esquerda\n");
+    };
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
